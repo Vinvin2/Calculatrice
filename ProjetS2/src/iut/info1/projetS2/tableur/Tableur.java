6c9992b3a5146bd6391f63594dff3672b08a5d1c
mickael <mickael.peries@gmail.com>
2015-05-06 14:18:42 +0200

    Taille Tableur

--- ProjetS2/src/iut/info1/projetS2/tableur/Tableur.java
+++ ProjetS2/src/iut/info1/projetS2/tableur/Tableur.java
@@ -192,7 +192,7 @@ public class Tableur extends JFrame{
         scroll = new JScrollPane(tableur);
         
         // Taille du tableur
-        scroll.setPreferredSize(new Dimension(700, 500));
+        scroll.setPreferredSize(new Dimension(740, 500));
         
         // Ajout des en-têtes de ligne à notre tableur
         scroll.setRowHeaderView(enteteLigne);
